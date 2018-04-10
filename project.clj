2d6918970f71b11e112e5cf8895b80ffae52cf6c
Niklas Fasching <niklas.fasching@gmail.com>
2018-04-10 20:42:30 +0200

    Deploy to clojars
    $ lein deploy clojars
    
    I want to use yawc as a dependency for headless and have the CI work (locally
    lein checkouts is fine).
    
    - change version to SNAPSHOT (to follow the convention of gpg signing
      non-snapshot builds, move to a snapshot)
    - add namespace org.clojars.niklasfasching (because yawc/yawc is not working
      and the org.clojars.niklasfasching namespace already exists)

--- project.clj
+++ project.clj
@@ -1,6 +1,2 @@
-(defproject yawc "0.0.1"
-  :description "bare-bones websocket client"
-  :dependencies [[org.clojure/clojure "1.9.0"]]
-  :main ^:skip-aot yawc.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+(defproject org.clojars.niklasfasching/yawc "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.9.0"]])
